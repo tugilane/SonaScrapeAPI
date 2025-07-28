b1e1af9e5ea436dd9e6b7f4fac7f6ab7c3994817
tugilane <marten.peljo@gmail.com>
2025-07-28 18:32:24 +0300

    päringu aeg ja tulemused töökorras

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ import datetime
 import zoneinfo
 import bs4
 import lxml
+import string
 
-def scrape_postimees(sona):
+def scrape_err(sona):
 
     leht = requests.get('https://err.ee')
     html = leht.text
     soup = bs4.BeautifulSoup(html, 'lxml')
 
+    pealkirjaList = []
+    sonuKokku = 0
     for artikkel in soup.find_all(class_="article"):
         pealkiri = artikkel.find("h2")
+
         if pealkiri is not None:
-            print(pealkiri.get_text())
+            artikkelTekst = pealkiri.get_text().lower()
+            märgitaTekst = artikkelTekst.translate(str.maketrans('', '', string.punctuation))
+            sonadeArv = märgitaTekst.split().count(sona.lower())
+            if sonadeArv > 0:
+                sonuKokku += sonadeArv
+                pealkirjaList.append(artikkelTekst)
 
     aegEestis = datetime.datetime.now(zoneinfo.ZoneInfo('Europe/Helsinki'))
+
     print(aegEestis.isoformat())
+    print(sonuKokku)
+    print(pealkirjaList)
 
 
 
-scrape_postimees('tere')
+
+scrape_err('trump')
 
 
