f617a0756bf8e1ba410dfe100e7eb961e41193dd
tugilane <marten.peljo@gmail.com>
2025-07-29 06:23:40 +0300

    tavaline storage blob peaaegu valmis, kuid proovin append blob luua selle asemel

--- main.py
+++ main.py
@@ -21,8 +21,13 @@ def blob_tulemuste_nimekiri():
     blob_data = blob_client.download_blob().readall()
     return json.loads(blob_data)
 
+def blob_ules_laadimine(data):
+    container_client = blob_service_client.get_container_client(container="tulemused")
+    blob_client = container_client.get_blob_client("tulemused.json")
+    blob_client.upload_blob(data,)
+
 @app.route('/tulemused', methods=['GET'])
-def tulemused():
+def vaata_tulemusi():
     try:
         data = blob_tulemuste_nimekiri()
         print(data)
@@ -31,29 +36,49 @@ def tulemused():
         print("Error:", e)
         return jsonify({"error": str(e)}), 500
 
-def scrape_err(sona):
-    leht = requests.get('https://err.ee')
-    html = leht.text
-    soup = bs4.BeautifulSoup(html, 'lxml')
+@app.route('/tulemused', methods=['POST'])
+def lisa_tulemus():
 
-    pealkirjaList = []
-    sonuKokku = 0
-    for artikkel in soup.find_all(class_="article"):
-        pealkiri = artikkel.find("h2")
+    input = json.loads(request.data)
+    sona = input['otsitav']
+    try:
 
-        if pealkiri is not None:
-            artikkelTekst = pealkiri.get_text().lower()
-            märgitaTekst = artikkelTekst.translate(str.maketrans('', '', string.punctuation))
-            sonadeArv = märgitaTekst.split().count(sona.lower())
-            if sonadeArv > 0:
-                sonuKokku += sonadeArv
-                pealkirjaList.append(artikkelTekst)
+        leht = requests.get('https://err.ee')
+        html = leht.text
+        soup = bs4.BeautifulSoup(html, 'lxml')
 
-    aegEestis = datetime.datetime.now(zoneinfo.ZoneInfo('Europe/Helsinki'))
+        pealkirjaList = []
+        sonuKokku = 0
+        for artikkel in soup.find_all(class_="article"):
+            pealkiri = artikkel.find("h2")
 
-    print(aegEestis.isoformat())
-    print(sonuKokku)
-    print(pealkirjaList)
+            if pealkiri is not None:
+                artikkelTekst = pealkiri.get_text().lower()
+                märgitaTekst = artikkelTekst.translate(str.maketrans('', '', string.punctuation))
+                sonadeArv = märgitaTekst.split().count(sona.lower())
+                if sonadeArv > 0:
+                    sonuKokku += sonadeArv
+                    pealkirjaList.append(artikkelTekst)
+
+        aegEestis = datetime.datetime.now(zoneinfo.ZoneInfo('Europe/Helsinki'))
+
+        print(aegEestis.isoformat())
+        print(sonuKokku)
+        print(pealkirjaList)
+
+        data = {
+            "sona": sona,
+            "sonuKokku": sonuKokku,
+            "aeg": aegEestis.isoformat(),
+            "pealkirjaList": pealkirjaList
+        }
+
+        json_data = json.dumps(data)
+        blob_ules_laadimine(json_data)
+        return ('rida lisatud', 200)
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
