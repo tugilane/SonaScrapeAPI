9a5f3b3c7e878e687747918d65e80597abb0ea59
tugilane <marten.peljo@gmail.com>
2025-11-22 16:03:34 +0200

    post(v4.6) päring töötab uue sandÃ andmebaasi süsteemiga, vaja parandada get päring

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/tulemused/*": {"origins": "*"}}) # Origins määrata frontend lehe aadress?
+cors = CORS(app, resources={r"/tulemused*": {"origins": "*"}}) # Origins määrata frontend lehe aadress?
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["100 per day", "50 per hour", "1 per second"],
+    default_limits=["100 per day", "50 per hour", "1 per second"], #üldine limiit mõlemale endpointile
     storage_uri="memory://",
 )
 
@@ -36,19 +36,29 @@ def blob_lae_alla_json(failiNimi):
     return json_data
 
 # uue otsingu data üles laadimine, uue JSON failina.
-def blob_ules_laadimine(data, failiJarjekord, uusFail):
+def blob_ules_laadimine(uus_data, failiJarjekord, uusFail):
+    print("jõudsin üles laadimiseni")
     fail = failiJarjekord + ".json" # selgitame mis faili saadame uue päringu
     blob_client = blob_service_client.get_blob_client(container="tulemused", blob=fail)
 
     if uusFail: # kui päring läheb uude faili, loome ka selle faili
-        blob_client.upload_blob(data, overwrite=True)
-    else: # kui ei lähe uude faili, lisame käesolevasse
-        blob_client.upload_blob(data)
+        dataList = []
+        dataList.append(uus_data)
+        json_data = json.dumps(dataList)
+        blob_client.upload_blob(json_data)
+    else: # kui ei läheb uude faili, lisame käesolevasse
+        json_raw = blob_lae_alla_json(failiJarjekord + ".json")
+        json_raw.append(uus_data)
+        json_data = json.dumps(json_raw)
+        blob_client.upload_blob(json_data, overwrite=True)
+        print("tegin töö")
 
 #selgitame kus maal on järjekord
 def blob_lae_alla_jarjekord():
     blob_client = blob_service_client.get_blob_client(container="jarg", blob="jarjekord.txt") #järjefail
-    return blob_client.download_blob().readall()
+    jarjekordBinaar = blob_client.download_blob().readall()
+    jarjekord = jarjekordBinaar.decode("utf-8")
+    return jarjekord
 
 #laeme uue järjekorra üles
 def blob_lae_ules_jarjekord(jarjekorraFail):
@@ -114,25 +124,32 @@ def lisa_tulemus():
             "artikliteLingid": artikliteLingid,
         }
 
-        json_data = json.dumps(data)
-
         try:
             #vaatame järjekorda ja uuendame järge
-            jarjekord = blob_lae_alla_jarjekord().split(",") # failis esimene arv on käesolev fail ja teine arv on päringute arv failis
+            try:
+                jarjekord = blob_lae_alla_jarjekord().split(",") # failis esimene arv on käesolev fail ja teine arv on päringute arv failis
+            except Exception as e:
+                return jsonify({"message": "viga jarjekorra allalaadimisel" + str(e)}), 500
             uusFail = False  # kas vaja uus fail luua
-            if jarjekord[1] == 15: # kui on 15 päringut failis
-                jarjekord[0] = jarjekord[0] + 1 # muudame uueks faililaiendiks ühe võrra suurema arvu
-                jarjekord[1] = 0 # nullime päringute arvu failis
+            leht = int(jarjekord[0])
+            päringuid = int(jarjekord[1])
+            if päringuid >= 15: # kui on 15 päringut failis
+                leht = leht + 1 # muudame uueks faililaiendiks ühe võrra suurema arvu
+                päringuid = 0 # nullime päringute arvu failis
                 uusFail = True
             else:
-                jarjekord[1] = jarjekord[1] + 1 # kui pole veel 15 päringut, lisama päringute arvule +1
-            uuendatudJärjekord = jarjekord[0] + jarjekord[1]
+                päringuid = päringuid + 1 # kui pole veel 15 päringut, lisama päringute arvule +1
 
-            blob_lae_ules_jarjekord(uuendatudJärjekord) #laeme üles uue järjekorra
+            uuendatudJärjekord = str(leht) + "," + str(päringuid)
+            try:
+                blob_lae_ules_jarjekord(uuendatudJärjekord) #laeme üles uue järjekorra
+            except Exception as e:
+                return jsonify({"message": "viga jarjekorra üles laadimisel" + str(e)}), 500
+
         except Exception as e:
-            return jsonify({"message": "viga järjekorra failiga suhtluses " + str(e)}), 500
+            return jsonify({"message": "viga järjekorra faili loogikas " + str(e)}), 500
 
-        blob_ules_laadimine(json_data, jarjekord[0], uusFail) # laeme üles uue päringu
+        blob_ules_laadimine(data, str(leht), uusFail) # laeme üles uue päringu
         return ({'message': 'rida lisatud'}, 201)
     except Exception as e:
         print("Error:", e)
